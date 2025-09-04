086d4f5769c07e1bc0c513de5869b9fcc46efce1
youngnam81-kim <kimyn81@naver.com>
2025-09-04 15:49:36 +0900

    ..

---
+++ src/ch022/LongExample.java
@@ -0,0 +1,18 @@
+package java2509.as;
+
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;		
+		long var2 = 20L;		
+		//long var3 = 1000000000000;  //컴파일러는 int로 간주하기 때문에 에러 발생시킴
+		long var4 = 1000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	} 
+}
+
+
+
+
