16f92b48105f462cdd3fc821755051eeab563dda
youngnam81-kim <kimyn81@naver.com>
2025-09-09 16:47:59 +0900

    src/ch07

--- src/ch07/sec05/exam01/Member.java
+++
@@ -1,4 +0,0 @@
-package ch07.sec05.exam01;
-
-public final class Member {
-}
--- src/ch07/sec05/exam01/VeryImportantPerson.java
+++
@@ -1,5 +0,0 @@
-package ch07.sec05.exam01;
-
-//public class VeryImportantPerson extends Member {
-public class VeryImportantPerson {
-}
